c41f681e62657f65c876b77ff96024535b3af58c
AnonymerAlgorithmiker <linus_diepold@web.de>
2024-07-08 14:50:52 +0200

    finished StatCollector

--- StatCollector/main.cpp
+++ StatCollector/main.cpp
@@ -7,10 +7,10 @@ namespace fs = std::filesystem;
 
 int main() {
 
-    //printStatsPerProblem(outputFile,planDir);
-    //printStatsPerDomain(outputFile,planDir);
-    //printStatsMultipleDomains(outputFile,planDir,true);
-    //printCompleteStatsMultipleDomains(config::problemFile,config::average_obdFile,config::averageFile,config::planDir);
+    printStatsPerProblem(outputFile,planDir);
+    printStatsPerDomain(outputFile,planDir);
+    printStatsMultipleDomains(outputFile,planDir,true);
+    rintCompleteStatsMultipleDomains(config::problemFile,config::average_obdFile,config::averageFile,config::planDir);
     compareStats(true);
     return 0;
 }
